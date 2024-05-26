c8ad1abfac614fb078d04b479d8e45ef09a64f41
Antoneli <105890114+devantoneli@users.noreply.github.com>
2024-05-26 19:35:27 -0300

    Iniciando o desenvolvimento

---
+++ dominkMoveis/js/script.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    // FUNÇÃO DE APARECER OS MÓVEIS
+    let luzes = document.getElementById("luzes");
+    luzes.style.opacity = 1;
+    luzes.style.top = "-110px";
+    //SOFÁ
+    let sofa = document.getElementById("sofa");
+    sofa.style.opacity = 1;
+    sofa.style.top = "190px";
+    sofa.style.width = "900px";
+    //MESA
+    let mesa = document.getElementById("mesa");
+    mesa.style.opacity = 1;
+    mesa.style.right = "300px";
+  };
